2553c5d151eb65dd36402924c15d021030d1292f
Alejandro Madrigal Leiva <me@alemadlei.tech>
2020-11-20 00:21:18 -0600

    Updated libraries.

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Declares libraries to install.
-PACKAGES="build-essential zlib1g-dev make automake autoconf libxml2-dev linux-headers-$(uname -r)"
+PACKAGES="build-essential zlib1g-dev make automake autoconf libxml2-dev libssl-dev libcurl4-openssl-dev linux-headers-$(uname -r)"
